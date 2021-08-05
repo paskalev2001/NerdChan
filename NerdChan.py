3ec79d679004817325523ba401f2e9a2e4defa1a
Paskal Paskalev <33760383+paskalev2001@users.noreply.github.com>
2021-08-05 16:56:19 +0300

    Implemented Sphinx Sentence Recognition

--- NerdChan.py
+++ NerdChan.py
@@ -1,6 +1,6 @@
 import wolframalpha
 client = wolframalpha.Client("Get your own key at wolframalpha")
-
+import speech_recognition as sr
 import wikipedia
 import speech_recognition as sr
 
@@ -18,6 +18,23 @@ engine = pyttsx3.init()
 engine.setProperty('rate', 220)
 engine.setProperty('voice', 'english+f3')
 
+def voiceRec():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("Say something!")
+        audio = r.listen(source)
+
+    # recognize speech using Sphinx
+    try:
+        voice_out = r.recognize_sphinx(audio)
+        print("Sphinx thinks you said " + voice_out)
+        return voice_out
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
 
 
 def wikiWolf():
@@ -25,7 +42,8 @@ def wikiWolf():
         event, values = window.read()
         if event in (None, 'Close'):
             break
-        
+        if event == 'Voice':
+            values[0] = voiceRec()
         try:
             wiki_res = wikipedia.summary(values[0], sentences=3)
             wolfram_res = next(client.query(values[0]).results).text
