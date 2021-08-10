cc354981da3c724b7a20877d8a4670031b58bd25
p.paskalev <p.paskalev@hyperaspect.com>
2021-08-10 15:47:14 +0300

    Config Reading

--- NerdChan.py
+++ NerdChan.py
@@ -1,22 +1,21 @@
 import wolframalpha
-client = wolframalpha.Client("Get your own key at wolframalpha")
 import speech_recognition as sr
 import wikipedia
 import speech_recognition as sr
-
+import pyttsx3
 import PySimpleGUI as sg
 
-sg.theme('DarkBlue4')
-layout =[
-    [sg.Text('Ask me something :)'), sg.InputText()],
-    [sg.Button('Ask'), sg.Button('Close'),sg.Button('Voice')]
-    ]
-window = sg.Window('NerdChan', layout)
-
-import pyttsx3
-engine = pyttsx3.init()
-engine.setProperty('rate', 220)
-engine.setProperty('voice', 'english+f3')
+def readConfig(name):
+    configFile = open("./config/"+name,'r')
+    configLines = configFile.readlines()
+    config = {}
+    for line in configLines:
+        line = line.replace('\n','')
+        kvp = line.split('=')
+        config[kvp[0]] = kvp[1]
+    if(config['apiKey']==''):
+        sg.PopupNonBlocking("Warning: Wolfram api key is not set therefore NerdChan will only return results from wikipedia. \n Visit https://developer.wolframalpha.com/portal/myapps/ to get one and write it in the config file.")
+    return config
 
 def voiceRec():
     # obtain audio from the microphone
@@ -36,8 +35,7 @@ def voiceRec():
     except sr.RequestError as e:
         print("Sphinx error; {0}".format(e))
 
-
-def wikiWolf():
+def wikiWolf(currentConfig):
     while True:
         event, values = window.read()
         if event in (None, 'Close'):
@@ -45,7 +43,7 @@ def wikiWolf():
         if event == 'Voice':
             values[0] = voiceRec()
         try:
-            wiki_res = wikipedia.summary(values[0], sentences=3)
+            wiki_res = wikipedia.summary(values[0], sentences=currentConfig["wikiLenght"])
             wolfram_res = next(client.query(values[0]).results).text
             engine.say(wolfram_res)
             sg.PopupNonBlocking("Wolfram Result: "+wolfram_res,"Wikipedia Result: "+wiki_res)
@@ -58,13 +56,29 @@ def wikiWolf():
             engine.say(wolfram_res)
             sg.PopupNonBlocking(wolfram_res)
         except:
-            wiki_res = wikipedia.summary(values[0], sentences=3)
+            wiki_res = wikipedia.summary(values[0], sentences=currentConfig["wikiLenght"])
             engine.say(wiki_res)
             sg.PopupNonBlocking(wiki_res)
 
         engine.runAndWait()
 
-        print (values[0])
+        #print (values[0])
 
     window.close()
-wikiWolf()
+
+
+currentConfig = readConfig("Default.config")
+
+client = wolframalpha.Client(currentConfig["apiKey"])
+sg.theme(currentConfig["theme"])
+layout =[
+    [sg.Text('Ask me something :)'), sg.InputText()],
+    [sg.Button('Ask'), sg.Button('Close'),sg.Button('Voice')]
+    ]
+window = sg.Window('NerdChan', layout)
+
+engine = pyttsx3.init()
+engine.setProperty('rate', currentConfig["rate"])
+engine.setProperty('voice', currentConfig["voice"])
+
+wikiWolf(currentConfig)
